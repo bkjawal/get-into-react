f5259a12a4ef844944d837edcbecfe4baad071c2
bkjawal <bkumarbt@gmail.com>
2016-02-04 09:23:21 +0530

    static ui component

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import reactDOM from 'react-dom';
 
 
-let Welcome = ()=> <div>Welcome to react world  with es6 arrow function:-)</div>
+//let Welcome = ()=> <div>Welcome to react world  with es6 arrow function:-)</div>;
+
+class Welcome extends React.Component{
+   constructor(){
+       super();
+   }
+   render(){
+       return (<div>First static component</div>);
+   }
+}
+
 
 reactDOM.render(
   <Welcome></Welcome>,
   document.getElementById('app')
 );
-
-
-
-
